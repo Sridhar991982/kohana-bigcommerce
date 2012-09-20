dc45b60d4e77348f9c88fa33dfe4004191e095e5
Robert Riggen <rob@riggen.org>
2012-09-20 11:26:27 -0400

    initial file import

---
+++ classes/bigcommerce.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Bigcommerce extends Kohana_Bigcommerce{}
---
+++ config/bigcommerce.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'store_url' => 'https://store.flyinghighcoffee.com/',
+    'username'    => 'admin',
+    'api_key'    => 'b32e1947173383b8d73f68145d3d6641'
+);
